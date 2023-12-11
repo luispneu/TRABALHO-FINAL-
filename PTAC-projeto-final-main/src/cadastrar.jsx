4172275062e52bed9d85e88f478c67680546fc63
luispneu <luisgabriel8328@gmail.com>
2023-12-11 08:27:11 -0400

    Cadastrar
    cadastrar adiconando na home

--- PTAC-projeto-final-main/src/cadastrar.jsx
+++ PTAC-projeto-final-main/src/cadastrar.jsx
@@ -21,7 +21,7 @@ export default function cadastrar() {
   }
   return (
     <div>
-    <div className    ="peblou">
+    <div className="peblou">
       <h1>Videos</h1>
       <form onSubmit={salvar}>
         <h2>Canal</h2>
@@ -30,13 +30,12 @@ export default function cadastrar() {
         <input onChange={(e) => setDescricao(e.target.value)} type="text" />
         <h2>Nome do video</h2>
         <input onChange={(e)=> setVideo(e.target.value)} type="text/>"/>
-        <h2                                                                                                                                  >URL</h2>
+        <h2                                                                                                                         >URL</h2>
         <input onChange={(e)=> setUrl(e.target.value)} type="text/>"/>
         <button>Incrementar</button>
       </form>
     </div>
     <div>
-    <iframe width="1280" height="720" src=  "https://www.youtube.com/embed/" title="Tang está aqui com mais nutrientes." frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
     </div>
     </div>
   );
