2230a00fdfd12ac951b691ed259b140893c4e07c
luispneu <luisgabriel8328@gmail.com>
2023-12-11 09:37:04 -0400

    Update NavBar.jsx
    upadte navabr

--- PTAC-projeto-final-main/src/componentes/NavBar.jsx
+++ PTAC-projeto-final-main/src/componentes/NavBar.jsx
@@ -3,51 +3,52 @@ import { Link } from 'react-router-dom';
 export default function NavBar() {
   return (
     <div>
-      <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="white">
-        <div class="bg-white p-4">
-          <h5 className="text-body-emphasis h4">
-            <img
-              src="/img/botao-home.png"
-              style={{ width: '45px ', height: '40px', borderRadius: '50%', marginBottom: '15px' }}
-              alt="Botão Home"
-            />
-          </h5>
-          <h2>
-            <img
-              src="/img/pra-cima.png"
-              style={{ width: '45px', height: '40px', marginBottom: '15px' }}
-              alt="Ícone para cima"
-            />
-          </h2>
-          <h3>
-            <Link to="/cadastrar">
-              <img
-                src="/img/adicionar.png"
-                style={{ width: '45px', height: '40px' }}
-                alt="Botão Adicionar"
-              />
-            </Link>
-          </h3>
-          <span class="text-body-secondary"></span>
-        </div>
-      </div>
-      <nav class="navbar navbar-white bg-white fixed-top">
-        <div class="container-fluid">
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarToggleExternalContent"
-            aria-controls="navbarToggleExternalContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
+      <nav className="navbar navbar-white bg-white fixed-top">
+        <div className="container-fluid">
+          <div className="d-flex">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarToggleExternalContent"
+              aria-controls="navbarToggleExternalContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse" id="navbarToggleExternalContent" data-bs-theme="white">
+              <div className="bg-white p-4">
+                <h5 className="text-body-emphasis h4">
+                  <img
+                    src="/img/botao-home.png"
+                    style={{ width: '45px ', height: '40px', borderRadius: '50%', marginBottom: '15px' }}
+                    alt="Botão Home"
+                  />
+                </h5>
+                <h2>
+                  <img
+                    src="/img/pra-cima.png"
+                    style={{ width: '45px', height: '40px', marginBottom: '15px' }}
+                    alt="Ícone para cima"
+                  />
+                </h2>
+                <h3>
+                  <Link to="/cadastrar">
+                    <img
+                      src="/img/adicionar.png"
+                      style={{ width: '45px', height: '40px' }}
+                      alt="Botão Adicionar"
+                    />
+                  </Link>
+                </h3>
+                <span className="text-body-secondary"></span>
+              </div>
+            </div>
+          </div>
         </div>
       </nav>
-      <h1 style={{ marginTop: '70px' }}>Ben's Videos</h1>
+      <h1 style={{ marginTop: '70px', paddingLeft: '20px' }}>Ben's Videos</h1>
     </div>
   );
 }
-  
